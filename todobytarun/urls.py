5f11731db5e332b6b58414d509757439725099bd
Tarun <68821990+tarun0786sharma@users.noreply.github.com>
2021-05-24 19:54:16 +0530

    Update urls.py

--- todobytarun/urls.py
+++ todobytarun/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('adminpanel/', admin.site.urls),
+    path('administrator/', admin.site.urls),
     path('', include('todoapp.urls'))
 ]
